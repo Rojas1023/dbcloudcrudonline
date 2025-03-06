577e26527f85ed23db0d2b33715fd301715bb4a2
Jhoan Rojas <157655074+Rojas1023@users.noreply.github.com>
2025-03-06 08:33:51 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,12 @@
 console.log("Test:");
+console.log(response.json);
+pool.connect()
+    .then(client => {
+        console.log("✅ Conexión a la base de datos establecida");
+        client.release();
+    })
+    .catch(err => console.error("❌ Error al conectar a la base de datos:", err.message));
+
 
 async function fetchAccounts() {
     try {
